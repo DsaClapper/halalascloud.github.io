3415905418224a508e30b7380321073e103a165f
DsaClapper <71221540+DsaClapper@users.noreply.github.com>
2024-03-30 04:31:23 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,48 @@
+const urlBase64ToUint8Array = base64String => {
+    const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+
+    const rawData = atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+
+    return outputArray;
+}
+
+const saveSubscription = async (subscription, email) => {
+    const response = await fetch('http://localhost:10000/save-subs', {
+        method: 'post',
+        headers: { 'Content-type': "application/json" },
+        body: JSON.stringify({
+            sub: subscription,
+            em: email
+    })
+    })
+    return response.json()
+}
+
 self.addEventListener("activate", async (e) => {
-    let sub = await self.registration.pushManager.subscribe({
+    const subscription = await self.registration.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: "BG_jzd40rAS03APRXVje5C9CDaTMLDpcckfHs5oA0umzvh-9UYUybphcjsTiDQ-Ru1hPoUEmYwvGDrihjYuarv4"
-    });
+        applicationServerKey: urlBase64ToUint8Array("BG_jzd40rAS03APRXVje5C9CDaTMLDpcckfHs5oA0umzvh-9UYUybphcjsTiDQ-Ru1hPoUEmYwvGDrihjYuarv4")
+    })
+    let open = indexedDB.open("Hls Db", 1)
+    open.onsuccess = async function(event) {
+        let db = open.result
+        let transaction = db.transaction("user", "readwrite");
+        let objectStore = transaction.objectStore("user");
+        let em = objectStore.get("ssn").source.indexNames[0]
+        
+    const response = await saveSubscription(subscription, em)
+    console.log(response)
+    }
 })
 
 self.addEventListener("push", e => {
-    self.registration.showNotification("Wow", {
-        body: e.data.text()
-    })
+    self.registration.showNotification("Wohoo!!", { body: e.data.text() })
 })
